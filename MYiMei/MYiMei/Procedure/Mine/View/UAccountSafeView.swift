8eb0dd72658b67c19a054b41c32ec42928760f0c
于亿鑫 <>
2019-07-04 12:01:58 +0800

    账号与安全页面测试

--- MYiMei/MYiMei/Procedure/Mine/View/UAccountSafeView.swift
+++ MYiMei/MYiMei/Procedure/Mine/View/UAccountSafeView.swift
@@ -11,14 +11,37 @@ import UIKit
 class UAccountSafeView: BaseView {
     //MARK:申明各种控件变量
     
+    var bindPhoneLabel = UILabel()
     
+    var phoneNumberLabel = UILabel()
+    
+    var bindLine = UIView()
+    
+    var loginAccountLabel = UILabel()
+    
+    var accountNameLabel = UILabel()
+    
+    var accountLine = UIView()
+    
+    var changePasswordLabel = UILabel()
+    
+    var rightArrow = UIImageView()
     
     
     
     
     //MARK:初始化试图控件
     func configUI() {
-        
+        bindPhoneLabel.text = "绑定手机"
+        bindPhoneLabel.textColor = UIColor.black
+        bindPhoneLabel.font = UIFont.systemFont(ofSize: 14)
+        self.addSubview(bindPhoneLabel)
+        bindPhoneLabel.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.left.equalToSuperview().offset(15)
+            ConstraintMaker.top.equalToSuperview().offset(20)
+            ConstraintMaker.right.equalToSuperview()
+            ConstraintMaker.width.equalToSuperview()
+        }
     }
     
 }
