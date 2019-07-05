2dcf5c1f5e3b11db13e4be72b0568187eeeb83dc
于亿鑫 <>
2019-07-05 09:26:11 +0800

    账号与安全页面

--- MYiMei/MYiMei/Procedure/Mine/View/UAccountSafeView.swift
+++ MYiMei/MYiMei/Procedure/Mine/View/UAccountSafeView.swift
@@ -11,37 +11,132 @@ import UIKit
 class UAccountSafeView: BaseView {
     //MARK:申明各种控件变量
     
-    var bindPhoneLabel = UILabel()
+    //白色背景
+    var backgroundWhite = UIImageView()
     
+    //绑定手机
+    var bindPhoneLabel = UILabel()
     var phoneNumberLabel = UILabel()
     
-    var bindLine = UIView()
-    
+    //登录账号
     var loginAccountLabel = UILabel()
-    
     var accountNameLabel = UILabel()
     
-    var accountLine = UIView()
-    
+    //修改密码
     var changePasswordLabel = UILabel()
-    
     var rightArrow = UIImageView()
     
+    //灰色线
+    var bindLine = UIView()
+    var accountLine = UIView()
     
-    
+    var submitButton = UIButton()
     
     //MARK:初始化试图控件
     func configUI() {
-        bindPhoneLabel.text = "绑定手机"
-        bindPhoneLabel.textColor = UIColor.black
-        bindPhoneLabel.font = UIFont.systemFont(ofSize: 14)
-        self.addSubview(bindPhoneLabel)
-        bindPhoneLabel.snp.makeConstraints { (ConstraintMaker) in
-            ConstraintMaker.left.equalToSuperview().offset(15)
-            ConstraintMaker.top.equalToSuperview().offset(20)
-            ConstraintMaker.right.equalToSuperview()
+        self.backgroundColor = UIColor.hex(hexString: "#F5F5F5")
+        
+        
+        //MARK:背景颜色
+        self.addSubview(backgroundWhite)
+        backgroundWhite.backgroundColor = UIColor.white
+        backgroundWhite.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.top.equalToSuperview()
+            ConstraintMaker.height.equalTo(148)
             ConstraintMaker.width.equalToSuperview()
         }
+        
+        //MARK:绑定手机
+        self.addSubview(bindPhoneLabel)
+        bindPhoneLabel.text = "绑定手机"
+        bindPhoneLabel.textColor = UIColor.black
+        bindPhoneLabel.font = UIFont.systemFont(ofSize: 17)
+        bindPhoneLabel.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.left.equalToSuperview().offset(15)
+            ConstraintMaker.top.equalTo(backgroundWhite.snp.top).offset(14)
+        }
+        
+        //MARK:手机号
+        self.addSubview(phoneNumberLabel)
+        phoneNumberLabel.text = "13265345992"
+        phoneNumberLabel.textColor = UIColor.hex(hexString: "#666666")
+        phoneNumberLabel.font = UIFont.systemFont(ofSize: 17)
+        phoneNumberLabel.snp.makeConstraints { (ConstraintMaker) in
+           ConstraintMaker.top.equalTo(backgroundWhite.snp.top).offset(14)
+            ConstraintMaker.right.equalToSuperview().offset(-15)
+        }
+        
+        //MARK:绑定手机分割线
+        self.addSubview(bindLine)
+        bindLine.backgroundColor = UIColor.hex(hexString: "#E5E5E5")
+        bindLine.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.height.equalTo(1)
+            ConstraintMaker.top.equalTo(bindPhoneLabel.snp.bottom).offset(14)
+            ConstraintMaker.right.equalToSuperview().offset(-15)
+            ConstraintMaker.left.equalToSuperview().offset(15)
+        }
+        
+        self.addSubview(loginAccountLabel)
+        loginAccountLabel.text = "登录账号"
+        loginAccountLabel.textColor = UIColor.black
+        loginAccountLabel.font = UIFont.systemFont(ofSize: 17)
+        loginAccountLabel.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.left.equalToSuperview().offset(15)
+            ConstraintMaker.top.equalTo(bindLine.snp.bottom).offset(14)
+        }
+        
+        self.addSubview(accountNameLabel)
+        accountNameLabel.text = "yuyixin123456"
+        accountNameLabel.textColor = UIColor.hex(hexString: "#666666")
+        accountNameLabel.font = UIFont.systemFont(ofSize: 17)
+        accountNameLabel.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.top.equalTo(bindLine.snp.bottom).offset(14)
+            ConstraintMaker.right.equalToSuperview().offset(-15)
+        }
+        
+        self.addSubview(accountLine)
+        accountLine.backgroundColor = UIColor.hex(hexString: "#E5E5E5")
+        accountLine.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.height.equalTo(1)
+            ConstraintMaker.top.equalTo(loginAccountLabel.snp.bottom).offset(14)
+            ConstraintMaker.right.equalToSuperview().offset(-15)
+            ConstraintMaker.left.equalToSuperview().offset(15)
+        }
+        
+        self.addSubview(changePasswordLabel)
+        changePasswordLabel.text = "修改手机"
+        changePasswordLabel.textColor = UIColor.black
+        changePasswordLabel.font = UIFont.systemFont(ofSize: 17)
+        changePasswordLabel.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.left.equalToSuperview().offset(15)
+            ConstraintMaker.top.equalTo(accountLine.snp.bottom).offset(14)
+        }
+        
+        self.addSubview(rightArrow)
+        rightArrow.image = UIImage.init(named: "notice")
+        rightArrow.snp.makeConstraints { (ConstraintMaker) in
+            ConstraintMaker.height.equalTo(14)
+            ConstraintMaker.width.equalTo(9)
+            ConstraintMaker.top.equalTo(accountLine.snp.bottom).offset(14)
+            ConstraintMaker.right.equalToSuperview().offset(-15)
+        }
+        
+        self.addSubview(submitButton)
+        submitButton.setTitle("退出登录", for: .normal)
+        submitButton.titleLabel?.textColor = UIColor.white
+        submitButton.clipsToBounds = true
+        submitButton.layer.cornerRadius = 3
+        submitButton.backgroundColor = UIColor.hex(hexString: "#1C98F6")
+        submitButton.snp.makeConstraints { (ConstraintMaker) in
+    
+            ConstraintMaker.left.equalToSuperview()
+            ConstraintMaker.right.equalToSuperview()
+            ConstraintMaker.bottom.equalToSuperview().offset(-10)
+            ConstraintMaker.height.equalTo(44)
+            ConstraintMaker.width.equalToSuperview()
+        }
+        
+        
     }
     
 }
--- MYiMei/MYiMei/Procedure/Mine/ViewController/UMineViewController.swift
+++ MYiMei/MYiMei/Procedure/Mine/ViewController/UMineViewController.swift
@@ -9,13 +9,13 @@
 import UIKit
 
 class UMineViewController: UBaseViewController {
-
+    
     fileprivate var service: APIUserServices = APIUserServices()
-
+    
     private lazy var myArray: Array = {
         return [
             [
-                ["icon":"account_security", "title": "账号与安全"]
+                ["icon":"account_security", "title": "店铺数据"]
             ],
             [
                 ["icon":"account_security", "title": "账号与安全"],
@@ -25,15 +25,15 @@ class UMineViewController: UBaseViewController {
             ]
         ]
     }()
-
+    
     private lazy var head: UMineHead = {
         return UMineHead(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 2))
     }()
-
+    
     private lazy var navigationBarY: CGFloat = {
         return navigationController?.navigationBar.frame.maxY ?? 0
     }()
-
+    
     lazy var tableView: UITableView = {
         let tw = UITableView(frame: .zero, style: .grouped)
         tw.backgroundColor = UIColor.background
@@ -43,25 +43,25 @@ class UMineViewController: UBaseViewController {
         tw.register(cellType: UStoreEventsCell.self)
         return tw
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func configUI() {
         view.addSubview(tableView)
         tableView.snp.makeConstraints {
             $0.edges.equalTo(self.view.usnp.edges).priority(.low)
             $0.top.equalToSuperview()
         }
-
+        
         tableView.parallaxHeader.view = head
         tableView.parallaxHeader.height = 2
         tableView.parallaxHeader.minimumHeight = navigationBarY
         tableView.parallaxHeader.mode = .fill
         tableView.separatorColor = UIColor.clear
     }
-
+    
     func goToGoodsManageMent(){
         let vc = UGoodsManagementController()
         vc.title = "商品管理"
@@ -76,11 +76,11 @@ class UMineViewController: UBaseViewController {
 }
 
 extension UMineViewController: UITableViewDelegate, UITableViewDataSource {
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return myArray.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if(indexPath.section == 0){
             return 250
@@ -88,20 +88,20 @@ extension UMineViewController: UITableViewDelegate, UITableViewDataSource {
             return 44
         }
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let sectionArray = myArray[section]
         return sectionArray.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return CGFloat.leastNormalMagnitude
     }
-
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return nil
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if(indexPath.section == 0){
             let cell = tableView.dequeueReusableCell(for: indexPath, cellType: UStoreEventsCell.self)
@@ -129,62 +129,64 @@ extension UMineViewController: UITableViewDelegate, UITableViewDataSource {
             cell.textLabel?.text = dict["title"]
             return cell
         }
-
+        
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-//        if indexPath.row == 2 {
-//            showAlertControllerStyle()
-//        }else if indexPath.row == 0{
-//            let vc = UModifyProfileViewController()
-//            let sectionArray = myArray[indexPath.section]
-//            let dict: [String: String] = sectionArray[indexPath.row]
-//            vc.title = dict["title"]
-//            navigationController?.pushViewController(vc, animated: true)
-//        }else {
-//            let vc = UModifyPasswordViewController()
-//            let sectionArray = myArray[indexPath.section]
-//            let dict: [String: String] = sectionArray[indexPath.row]
-//            vc.title = dict["title"]
-//            navigationController?.pushViewController(vc, animated: true)
-//        }
-
+        if indexPath.row == 0 {
+            let vc = UAccountSafeController()
+            vc.title = "账号与安全"
+            navigationController?.pushViewController(vc, animated: true)
+        }else if indexPath.row == 0{
+            //            let vc = UModifyProfileViewController()
+            //            let sectionArray = myArray[indexPath.section]
+            //            let dict: [String: String] = sectionArray[indexPath.row]
+            //            vc.title = dict["title"]
+            //            navigationController?.pushViewController(vc, animated: true)
+        }else {
+            //            let vc = UModifyPasswordViewController()
+            //            let sectionArray = myArray[indexPath.section]
+            //            let dict: [String: String] = sectionArray[indexPath.row]
+            //            vc.title = dict["title"]
+            //            navigationController?.pushViewController(vc, animated: true)
+        }
+        
     }
-
+    
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 20
     }
-
+    
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         return nil
     }
-
-
-
-//    func showAlertControllerStyle() {
-//        let alertController = UIAlertController(title: "温馨提示", message: "是否确定退出登录？", preferredStyle: UIAlertController.Style.alert)
-//        let okAction = UIAlertAction(title: "确定", style: UIAlertAction.Style.default) {
-//            (action: UIAlertAction!) -> Void in
-//            self.logout()
-//        }
-//        let cancelAction = UIAlertAction(title: "取消", style: UIAlertAction.Style.cancel, handler: nil)
-//        alertController.addAction(okAction)
-//        alertController.addAction(cancelAction)
-//        self.present(alertController, animated: true, completion: nil)
-//    }
-
-//    func logout() -> Void {
-//        let userId: String = U17User.shared.user?.userid ?? ""
-//        service.logout(userId: userId, {
-//            JPUSHService.deleteAlias({ (iResCode, iAlias, seq) in
-//                print("退出注销极光别名儿 \(iResCode),\(String(describing: iAlias)),\(seq)")
-//            }, seq: 0)
-//            (UIApplication.shared.delegate as! AppDelegate).showLoginView()
-//        }) { (FAPIErrorModel) in
-//            //            print(FAPIErrorModel.error?.localizedDescription ?? "退出失败")
-//            (UIApplication.shared.delegate as! AppDelegate).showLoginView()
-//        }
-//    }
+    
+    
+    
+    //    func showAlertControllerStyle() {
+    //        let alertController = UIAlertController(title: "温馨提示", message: "是否确定退出登录？", preferredStyle: UIAlertController.Style.alert)
+    //        let okAction = UIAlertAction(title: "确定", style: UIAlertAction.Style.default) {
+    //            (action: UIAlertAction!) -> Void in
+    //            self.logout()
+    //        }
+    //        let cancelAction = UIAlertAction(title: "取消", style: UIAlertAction.Style.cancel, handler: nil)
+    //        alertController.addAction(okAction)
+    //        alertController.addAction(cancelAction)
+    //        self.present(alertController, animated: true, completion: nil)
+    //    }
+    
+    //    func logout() -> Void {
+    //        let userId: String = U17User.shared.user?.userid ?? ""
+    //        service.logout(userId: userId, {
+    //            JPUSHService.deleteAlias({ (iResCode, iAlias, seq) in
+    //                print("退出注销极光别名儿 \(iResCode),\(String(describing: iAlias)),\(seq)")
+    //            }, seq: 0)
+    //            (UIApplication.shared.delegate as! AppDelegate).showLoginView()
+    //        }) { (FAPIErrorModel) in
+    //            //            print(FAPIErrorModel.error?.localizedDescription ?? "退出失败")
+    //            (UIApplication.shared.delegate as! AppDelegate).showLoginView()
+    //        }
+    //    }
 }
 
