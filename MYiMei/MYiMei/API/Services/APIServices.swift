074f8eb03a94bdd5f866aa560d624bffc073b256
KevinFu <814073395@qq.com>
2019-08-10 12:26:04 +0800

    拉取代码打包

--- MYiMei/MYiMei/API/Services/APIServices.swift
+++ MYiMei/MYiMei/API/Services/APIServices.swift
@@ -13,11 +13,11 @@ import MBProgressHUD
 
 let ssl: Bool = true
 //MARK:开发环境
- let baseHttpsUrl: String = "https://testjiajuapi.ifhu.cn"
- let baseHttpUrl: String = "http://testjiajuapi.ifhu.cn"
+// let baseHttpsUrl: String = "https://testjiajuapi.ifhu.cn"
+// let baseHttpUrl: String = "http://testjiajuapi.ifhu.cn"
 //MARK:正式环境
-//let baseHttpsUrl: String = "https://jiajuapi.ifhu.cn"
-//let baseHttpUrl: String = "http://jiajuapi.ifhu.cn"
+let baseHttpsUrl: String = "https://jiajuapi.ifhu.cn"
+let baseHttpUrl: String = "http://jiajuapi.ifhu.cn"
 
 // MARK: 网络请求加载插件
 let loadingPlugin = NetworkActivityPlugin { (type, target) in
